8e1dce7c6a5daf1f20cc6c7a6e5d15e9bfbe0f2c
Daniel Ranchal <danitico98@gmail.com>
2018-02-22 18:59:56 +0100

    First commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $tasks=[
+            'aprender symfony',
+            'ir a la academia',
+            'Jugar',
+        ];
+
+        foreach ($tasks as $task) {
+            $entity=new Task();
+            $entity->setDescription($task);
+
+            $manager->persist($entity);
+        }
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
